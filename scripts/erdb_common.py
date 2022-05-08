ee6c99b996ac4cbcb0b498a81fccfd21d03f0385
Phil25 <phil25@protonmail.com>
2022-05-08 13:19:07 +0200

    Add json schema validation for generating data

--- scripts/erdb_common.py
+++ scripts/erdb_common.py
@@ -1,15 +1,65 @@
 import json
 import collections
-from typing import List, Dict
+from typing import Tuple, Dict
+from jsonschema import validate, RefResolver, ValidationError
 
-def get_schema_properties(filename: str, path: List[str]=[]) -> Dict:
-    with open(f"./schema/{filename}.schema.json") as f:
-        schema = json.load(f)
+def load_schema(filename: str) -> Tuple[str, Dict]:
+    filename = f"{filename}.schema.json"
+    with open(f"./schema/{filename}", mode="r") as f:
+        return filename, json.load(f)
 
-    for part in path:
-        schema = schema[part]
+def get_schema_properties(*references: str) -> Tuple[Dict, Dict[str, Dict]]:
+    """
+    Parse and merge properties of individual schemas.
+    Takes in references to objects containing the "properties" field of a schema.
+    Becuase the properties get merged, the order of references matters.
 
-    return schema["properties"]
+    Format: "<base schema filename>/path/to/object", ex. "spirit-ashes/definitions/SpiritAsh"
+
+    Returns the merged property dict and schemas that were parsed in full as a filename->schema dict.
+    """
+    properties_full = {}
+    store = {}
+
+    for ref in references:
+        filename, *path = ref.split("/")
+        filename, schema = load_schema(filename)
+
+        obj = schema
+        for part in path:
+            obj = obj[part]
+
+        update_nested(properties_full, obj["properties"])
+        store[filename] = schema
+
+    return properties_full, store
+
+def patch_keys(obj: Dict, schema: Dict) -> Dict:
+    # delete excessive keys
+    for key in (set(obj.keys()) - set(schema.keys())):
+        del obj[key]
+
+    # add missing base keys
+    for key in (set(schema.keys()) - set(obj.keys())):
+        obj[key] = schema[key].get("default", {})
+    
+    return obj
+
+def validate_and_write(file_path: str, schema_name: str, data: Dict, store: Dict[str, Dict]) -> bool:
+    try:
+        resolver = RefResolver(base_uri="unused", referrer="unused", store=store)
+        validate(data, store[schema_name], resolver=resolver)
+
+    except ValidationError as e:
+        readable_path = "/".join(str(part) for part in e.path)
+        print(f"Failed to validate \"{readable_path}\": {e.message}")
+        return False
+
+    finally:
+        with open(file_path, mode="w") as f:
+            json.dump(data, f, indent=4, sort_keys=True)
+
+    return True
 
 # update function which recursively updates subdictionaries
 # borrowed from https://stackoverflow.com/a/3233356
--- scripts/generate_spirit_ashes.py
+++ scripts/generate_spirit_ashes.py
@@ -1,8 +1,8 @@
 import json
 import er_params
-from erdb_common import update_nested
 from typing import Dict, List
 from er_params.enums import GoodsType, GoodsRarity, ItemIDFlag
+from erdb_common import update_nested, get_schema_properties, patch_keys, validate_and_write
 
 ParamRow = er_params.ParamRow
 ParamDict = er_params.ParamDict
@@ -13,33 +13,31 @@ def is_base_spirit_ash(row: ParamRow) -> bool:
 def find_upgrade_costs(goods: ParamDict, base_item_id: int) -> List[int]:
     return [goods[str(item_id)].get_int("reinforcePrice") for item_id in range(base_item_id, base_item_id + 10)]
 
-def make_spirit_ash_object(row: ParamRow, goods: ParamDict, upgrade_mats: ParamDict) -> Dict[str, Dict]:
+def make_spirit_ash_object(row: ParamRow, goods: ParamDict, upgrade_mats: ParamDict) -> Dict:
     # HACK: reinforceMaterialId links to EquipMtrlSetParam which THEN links to the actual EquipParamGoods
     # this simply checks if the name of EquipMtrlSetParam somewhat matches without following everything
     upgrade_material = upgrade_mats[row.get("reinforceMaterialId")].name.startswith("Grave")
     upgrade_material = "Grave Glovewort" if upgrade_material else "Ghost Glovewort"
 
     return {
-        row.name: {
-            "full_hex_id": row.index_hex,
-            "id": row.index,
-            "name": row.name,
-            # summary -- cannot autogenerate, make sure not to overwrite
-            # description -- cannot autogenerate, make sure not to overwrite
-            "is_tradable": row.get("disableMultiDropShare") == "0",
-            "price_sold": row.get_int_corrected("sellValue"),
-            "max_held": row.get_int("maxNum"),
-            "max_stored": row.get_int("maxRepositoryNum"),
-            # locations -- cannot autogenerate, make sure not to overwrite
-            # remarks -- cannot autogenerate, make sure not to overwrite
-            "fp_cost": row.get_int_corrected("consumeMP"),
-            "hp_cost": row.get_int_corrected("consumeHP"),
-            "rarity": GoodsRarity(row.get_int("rarity")).name.lower(),
-            # summon_quantity -- cannot autogenerate, make sure not to overwrite
-            # abilities -- cannot autogenerate, make sure not to overwrite
-            "upgrade_material": upgrade_material,
-            "upgrade_costs": find_upgrade_costs(goods, row.index)
-        }
+        "full_hex_id": row.index_hex,
+        "id": row.index,
+        "name": row.name,
+        # summary -- cannot autogenerate, make sure not to overwrite
+        # description -- cannot autogenerate, make sure not to overwrite
+        "is_tradable": row.get("disableMultiDropShare") == "0",
+        "price_sold": row.get_int_corrected("sellValue"),
+        "max_held": row.get_int("maxNum"),
+        "max_stored": row.get_int("maxRepositoryNum"),
+        # locations -- cannot autogenerate, make sure not to overwrite
+        # remarks -- cannot autogenerate, make sure not to overwrite
+        "fp_cost": row.get_int_corrected("consumeMP"),
+        "hp_cost": row.get_int_corrected("consumeHP"),
+        "rarity": GoodsRarity(row.get_int("rarity")).name.lower(),
+        # summon_quantity -- cannot autogenerate, make sure not to overwrite
+        # abilities -- cannot autogenerate, make sure not to overwrite
+        "upgrade_material": upgrade_material,
+        "upgrade_costs": find_upgrade_costs(goods, row.index)
     }
 
 def main():
@@ -49,14 +47,21 @@ def main():
     spirit_ashes = spirit_ashes_full["SpiritAshes"]
     goods = er_params.load("EquipParamGoods", "1.04.1", ItemIDFlag.GOODS)
     upgrade_mats = er_params.load("EquipMtrlSetParam", "1.04.1", ItemIDFlag.NON_EQUIPABBLE)
+    properties, store = get_schema_properties("item", "spirit-ashes/definitions/SpiritAsh")
 
     for row in goods.values():
         if is_base_spirit_ash(row):
-            update_nested(spirit_ashes, make_spirit_ash_object(row, goods, upgrade_mats))
+            obj = make_spirit_ash_object(row, goods, upgrade_mats)
+            ash = spirit_ashes.get(row.name, {})
+
+            update_nested(ash, obj)
+            spirit_ashes[row.name] = patch_keys(ash, properties)
 
     spirit_ashes_full["SpiritAshes"] = spirit_ashes
-    with open("./spirit-ashes.json", mode="w") as f:
-        json.dump(spirit_ashes_full, f, indent=4)
+    ok = validate_and_write("./spirit-ashes.json", "spirit-ashes.schema.json", spirit_ashes_full, store)
+
+    if not ok:
+        raise RuntimeError("Validation failed")
 
 if __name__ == "__main__":
     main()
