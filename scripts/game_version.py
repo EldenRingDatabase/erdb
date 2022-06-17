95a767374766cc581292f4e7ae84a36c415e61a2
Phil25 <phil25@protonmail.com>
2022-06-18 00:48:19 +0200

    Add tests for GameVersion and GameVersionRange

--- scripts/game_version.py
+++ scripts/game_version.py
@@ -1,7 +1,9 @@
 import re
+from functools import total_ordering
 from pathlib import Path
 from typing import List, NamedTuple
 
+@total_ordering
 class GameVersion(NamedTuple):
     major: str
     minor: str
@@ -12,12 +14,14 @@ class GameVersion(NamedTuple):
     def from_string(cls: "GameVersion", version: str) -> "GameVersion":
         parts = version.split(".")
         assert len(parts) == 3, "Invalid version string given"
+        assert len(parts[1]) >= 2, "Minor part must be at least 2 digits"
+
         nums = [int(parts[0]), int(parts[1]), int(parts[2])]
         return cls(parts[0], parts[1], parts[2], nums)
 
     @classmethod
     def min(cls) -> "GameVersion":
-        return cls("0", "0", "0", [0, 0, 0])
+        return cls("0", "00", "0", [0, 0, 0])
 
     @classmethod
     def max(cls) -> "GameVersion":
@@ -36,51 +40,42 @@ class GameVersion(NamedTuple):
         return self.major == __o.major and self.minor == __o.minor and self.patch == __o.patch
 
     def __lt__(self, __o: "GameVersion") -> bool:
+        assert isinstance(__o, GameVersion)
         for this_num, other_num in zip(self.nums, __o.nums):
             if this_num != other_num:
                 return this_num < other_num
         return False
-
-    def __gt__(self, __o: "GameVersion") -> bool:
-        for this_num, other_num in zip(self.nums, __o.nums):
-            if this_num != other_num:
-                return this_num > other_num
-        return False
-
-    def __le__(self, __o: "GameVersion") -> bool:
-        for this_num, other_num in zip(self.nums, __o.nums):
-            if this_num != other_num:
-                return this_num < other_num
-        return True
-
-    def __ge__(self, __o: "GameVersion") -> bool:
-        for this_num, other_num in zip(self.nums, __o.nums):
-            if this_num != other_num:
-                return this_num > other_num
-        return True
 
 class GameVersionRange(NamedTuple):
     begin: GameVersion # including
-    end: GameVersion # excluding
+    end: GameVersion   # excluding
+    only: bool=False   # only `begin`
 
     @classmethod
     def from_string(cls: "GameVersionRange", string: str) -> "GameVersionRange":
         """
         Corresponds to /schema/userdata/version-range-pattern.schema.json
         """
+        def _ver(match: re.Match[str]) -> GameVersion:
+            return GameVersion.from_string(match.group(1))
+
         if string == "any version":
             return cls(GameVersion.min(), GameVersion.max())
 
-        search_begin = re.search(r"from (\d+\.\d+\.\d+)", string)
-        search_end = re.search(r"until (\d+\.\d+\.\d+)", string)
+        if search_only := re.search(r"only (\d+\.\d\d+\.\d+)", string):
+            return cls(_ver(search_only), GameVersion.max(), only=True)
+
+        search_begin = re.search(r"from (\d+\.\d\d+\.\d+)", string)
+        search_end = re.search(r"until (\d+\.\d\d+\.\d+)", string)
         assert search_begin or search_end, "Invalid version range string"
 
-        begin = GameVersion.from_string(search_begin.group(1)) if search_begin else GameVersion.min()
-        end = GameVersion.from_string(search_end.group(1)) if search_end else GameVersion.max()
+        begin = _ver(search_begin) if search_begin else GameVersion.min()
+        end = _ver(search_end) if search_end else GameVersion.max()
 
         assert begin < end, "Invalid version range string"
         return cls(begin, end)
 
     def __contains__(self, version: GameVersion) -> bool:
         assert isinstance(version, GameVersion)
-        return self.begin <= version and version < self.end
+        return self.begin == version if self.only \
+            else self.begin <= version and version < self.end
---
+++ tests/game_version_test.py
@@ -0,0 +1,91 @@
+from typing import List, Optional
+from scripts.game_version import GameVersion, GameVersionRange
+import pytest
+
+def _make(major: str, minor: str, patch: str) -> GameVersion:
+    assert len(minor) >= 2, "Minor part must be at least 2 digits"
+    return GameVersion(major, minor, patch, [int(major), int(minor), int(patch)])
+
+@pytest.fixture(scope="module")
+def base() -> GameVersion:
+    return _make("1", "02", "3")
+
+@pytest.mark.parametrize("left,right", [
+    (["1", "02", "3"], ["1", "02", "3"]),
+    (["1", "03", "3"], ["1", "03", "3"]),
+    (["3", "03", "3"], ["3", "03", "3"]),
+])
+def test_eq(left: List[str], right: List[str]):
+    assert _make(*left) == _make(*right)
+
+@pytest.mark.parametrize("left,right", [
+    (["1", "02", "4"], ["1", "02", "3"]),
+    (["1", "04", "3"], ["1", "03", "3"]),
+    (["4", "03", "3"], ["3", "03", "3"]),
+])
+def test_ne(left: List[str], right: List[str]):
+    assert _make(*left) != _make(*right)
+
+@pytest.mark.parametrize("left", [["1", "02", "2"], ["1", "01", "9"], ["0", "09", "9"]])
+def test_lt(base, left):
+    assert _make(*left) < base
+
+@pytest.mark.parametrize("left", [["1", "02", "4"], ["1", "03", "0"], ["2", "00", "0"]])
+def test_gt(base, left):
+    assert _make(*left) > base
+
+@pytest.mark.parametrize("left", [["1", "02", "2"], ["1", "02", "3"]])
+def test_le(base, left):
+    assert _make(*left) <= base
+
+@pytest.mark.parametrize("left", [["1", "02", "3"], ["1", "02", "4"]])
+def test_ge(base, left):
+    assert _make(*left) >= base
+
+@pytest.mark.parametrize("string,expected", [
+    ("1.04.1", _make("1", "04", "1")),
+    ("2.64.9", _make("2", "64", "9")),
+    ("0.03.9",  _make("0", "03", "9")),
+])
+def test_from_string(string: str, expected: GameVersion):
+    assert GameVersion.from_string(string) == expected
+
+@pytest.mark.parametrize("string,begin,end", [
+    ("from 1.04.1 until 1.05.0", _make("1", "04", "1"), _make("1", "05", "0")),
+    ("from 1.04.1", _make("1", "04", "1"), GameVersion.max()),
+    ("until 1.05.0", GameVersion.min(), _make("1", "05", "0")),
+    ("only 1.04.1", _make("1", "04", "1"), GameVersion.max()),
+    ("any version", GameVersion.min(), GameVersion.max()),
+])
+def test_range_from_string(string: str, begin: GameVersion, end: GameVersion):
+    version_range = GameVersionRange.from_string(string)
+    assert version_range.begin == begin
+    assert version_range.end == end
+
+@pytest.mark.parametrize("string,version", [
+    ("from 1.04.1 until 1.05.0", _make("1", "04", "1")),
+    ("from 1.04.1 until 1.05.0", _make("1", "04", "2")),
+    ("from 1.04.1 until 1.05.0", _make("1", "04", "99")),
+    ("from 1.04.1", _make("1", "04", "99")),
+    ("from 1.04.1", _make("1", "05", "0")),
+    ("until 1.05.0", _make("1", "04", "99")),
+    ("until 1.05.0", _make("1", "03", "0")),
+    ("only 1.05.0", _make("1", "05", "0")),
+    ("any version", _make("1", "04", "99")),
+    ("any version", _make("1", "05", "99")),
+])
+def test_range_contains(string: str, version: GameVersion):
+    assert version in GameVersionRange.from_string(string)
+
+@pytest.mark.parametrize("string,version", [
+    ("from 1.04.1 until 1.05.0", _make("1", "04", "0")),
+    ("from 1.04.1 until 1.05.0", _make("1", "05", "0")),
+    ("from 1.04.1 until 1.05.0", _make("1", "05", "99")),
+    ("from 1.04.1", _make("1", "04", "0")),
+    ("from 1.04.1", _make("1", "03", "99")),
+    ("until 1.05.0", _make("1", "05", "0")),
+    ("only 1.05.0", _make("1", "04", "99")),
+    ("only 1.05.0", _make("1", "05", "1")),
+])
+def test_range_not_contains(string: str, version: GameVersion):
+    assert version not in GameVersionRange.from_string(string)
