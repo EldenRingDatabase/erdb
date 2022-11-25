e0a47b99acaf105a9549722c52dfa4c5abb8594e
Phil25 <phil25@protonmail.com>
2022-11-25 09:57:04 +0100

    Add 'latest' as game version option for the API

--- src/erdb/app_api/common.py
+++ src/erdb/app_api/common.py
@@ -8,10 +8,13 @@ from erdb.typing.game_version import GameVersion
 from erdb.typing.api_version import ApiVersion
 
 
-GameVersionEnum = Enum("GameVersionEnum", {str(v).replace(".", "_"): str(v) for v in GAME_VERSIONS})
+GameVersionEnum = Enum("GameVersionEnum", {"latest": "latest"} | {str(v).replace(".", "_"): str(v) for v in GAME_VERSIONS})
 
 @cache
 def generate(api: ApiVersion, game_version: GameVersionEnum, table: Table) -> dict: # type: ignore
+    if game_version == GameVersionEnum.latest:
+        return generate(api, list(GameVersionEnum)[1], table)
+
     ver = GameVersion.from_string(game_version.value)
     return table.make_generator(ver).generate(api)
 
